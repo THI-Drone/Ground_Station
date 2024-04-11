d794b31b324f4635cfa484ccc0f1446a96a189b7
di-math <sidemuth705@googlemail.com>
2024-04-11 22:05:51 +0200

    Created initial webapp scaffold.

---
+++ web/frontend/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen bg-slate-900 flex justify-center items-center">
+      <h1 className="text-white">This is Ground Control to Major Tom...</h1>
+    </div>
+  );
+}
+
+export default App;
