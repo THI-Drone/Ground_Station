bfe042ece148b6729f3c8207ac806f66e6407d21
Simon Demuth <sidemuth705@googlemail.com>
2024-04-25 21:42:53 +0200

    Implemented MVP-stage of the Ground-Station-Webapp.

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,7 +1,9 @@
+import SimpleLog from "./components/SimpleLog";
+
 function App() {
   return (
-    <div className="w-screen h-screen bg-slate-900 flex justify-center items-center">
-      <h1 className="text-white">This is Ground Control to Major Tom...</h1>
+    <div className="w-screen h-screen p-1 bg-gray-100">
+      <SimpleLog />
     </div>
   );
 }
